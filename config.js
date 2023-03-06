854e8334c9daf3b55c1a478916faf2560f3b64da
vieclamkcn <tranthanhloan270921@gmail.com>
2023-03-06 15:28:49 +0300

    2

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc b một ngày vui vẻ và ý nghĩa. Mong điều đ
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = './1.jpg';
+const giftImage = './2.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
