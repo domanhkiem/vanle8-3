32891735309235a8e752d4b25ad4c8a51e167740
domanhkiem <domanhkiemnd@gmail.com>
2025-03-07 14:35:16 +0300

    1

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = '2023';
+const nameGirl = '2025';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 8/3';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc b một ngày vui vẻ và ý nghĩa. Mong điều đẹp nhất sẽ đến với b trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc c một ngày vui vẻ và ý nghĩa. Mong điều đẹp nhất sẽ đến với c trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = './2.jpg';
+const giftImage = './3.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
